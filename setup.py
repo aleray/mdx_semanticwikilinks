685a1fcb8c6083c9fff29ff8b4dabd8650162554
Alexandre Leray <alexandre@alexandreleray.com>
2012-04-21 18:30:41 +0200

    Moved mdx_semanticwikilinks into its own packages.
    Before it was part of the Active Archives wiki repository.

---
+++ setup.py
@@ -0,0 +1,26 @@
+#! /usr/bin/env python
+
+
+from setuptools import setup
+
+
+setup(
+    name='mdx_semanticwikilinks',
+    version='1.0',
+    author='Alexandre Leray',
+    author_email='alexandre@stdin.fr',
+    description='Python-Markdown extension to add support for semantic (wiki)links (RDFa)..',
+    url='http://activearchives.org/',
+    py_modules=['mdx_semanticwikilinks'],
+    install_requires=['Markdown>=2.0',],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: BSD License',
+        'Intended Audience :: Developers',
+        'Environment :: Web Environment',
+        'Programming Language :: Python',
+        'Topic :: Text Processing :: Filters',
+        'Topic :: Text Processing :: Markup :: HTML'
+    ]
+)
